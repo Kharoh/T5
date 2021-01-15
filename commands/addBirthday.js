80277c1e1a3e38755da68636dfc83f94358e2fd6
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2021-01-15 19:36:14 +0100

    added birthday notifications

--- commands/addBirthday.js
+++ commands/addBirthday.js
@@ -3,7 +3,8 @@ const birthdays = require("../birthdays")
 module.exports = ({ client, guild, config, db, message }, args) => {
     
     newBirthdays = db.get('birthdays')
-    newBirthdays.push({
+
+    birthday = {
         id: args[0],
         date: {
             month: parseInt(args[1]),
@@ -12,11 +13,24 @@ module.exports = ({ client, guild, config, db, message }, args) => {
             minute: parseInt(args[4]),
             seconde: parseInt(args[5])
         }
-    })
+    }
 
+    newBirthdays.push(birthday)
+
+
+    birthdaysPerMonth = db.get('birthdaysPerMonth')
+
+    birthdaysPerMonth[args[1] - 1].push(birthday)
+
+    birthdaysPerMonth[args[1] - 1].sort((a, b) => a.date.day - b.date.day)
+
+    db.set('birthdaysPerMonth', birthdaysPerMonth)
+
+    
     db.set('birthdays', newBirthdays)
 
     console.log(db.get('birthdays'))
+    
 
     message.channel.send('Notification ajoutée')
 }
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let birthdays = require('./birthdays')
 
 let guild
 let commands
+let birthdaysToLookFor
+let birthdaysPerMonth
 
 require('dotenv').config()
 
@@ -16,9 +18,38 @@ client.on('ready', () => {
     guild = client.guilds.cache.get(config.guildID)
     console.log(`Logged in as ${client.user.tag}`)
 
+    // birthdays = db.get('birthdays')
+
+    // db.delete('birthdaysPerMonth')
+
+    // birthdaysPerMonth = new Array(12).fill(null).map(() => [])
+
+    // birthdays.forEach(birth => {
+    //     birthdaysPerMonth[birth.date.month - 1].push(birth)
+    // })
+
+    // birthdaysPerMonth.forEach(month => month.sort((a, b) => a.date.day - b.date.day))
+
+    // db.set('birthdaysPerMonth', birthdaysPerMonth)
+
     // db.set('birthdays', birthdays)
 
     birthdaysPerMonth = db.get('birthdaysPerMonth')
+
+    birthdays = db.get('birthdays')
+
+    // birthdaysPerMonth[0].shift()
+
+    // birthdays.splice(5, 1)
+
+
+    // console.log(birthdaysPerMonth[0])
+
+    // console.log(birthdays)
+
+    // db.set('birthdays', birthdays)
+
+    // db.set('birthdaysPerMonth', birthdaysPerMonth)
 
     let now = new Date()
 
@@ -27,6 +58,8 @@ client.on('ready', () => {
     console.log(birthdaysToLookFor)
 
     commands = require('./commands/commands')
+
+    dateChecker()
 })
 
 client.on('message', (message) => {
@@ -46,7 +79,32 @@ client.on('message', (message) => {
 })
 
 let dateChecker = () => {
-    guild.channels.cache.get(config.channelID).send(`Joyeux anniversaire <@${'419840551781793802'}> <:tada:798513412869980190> <:partying_face:798513412869980190>`)
+    
+    let waitForBirthday = (birthday) => {
+        let now = new Date()
+        console.log(birthday.date.day === now.getDate() ? ( birthday.date.hour - now.getHours()) * 3600000 + (birthday.date.minute - now.getMinutes()) * 60000 +(birthday.date.seconde - now.getSeconds()) * 1000 : 86400000)
+        setTimeout(() => {
+            console.log('sending message')
+            console.log(birthday)
+            guild.channels.cache.get(config.channelID).send(`Joyeux anniversaire <@${birthday.id}> <:tada:798513412869980190> <:partying_face:798513412869980190>`)
+            birthdaysToLookFor.shift()
+        }, birthday.date.day === now.getDate() ? ( birthday.date.hour - now.getHours()) * 3600000 + (birthday.date.minute - now.getMinutes()) * 60000 +(birthday.date.seconde - now.getSeconds()) * 1000 : 86400000)
+    }
+
+    birthdaysToLookFor.unshift({
+        id: '419840551781793802',
+        date: {
+            month: 0,
+            day: 15,
+            hour: 19,
+            minute : 35,
+            seconde: 25
+        }
+    })
+    
+    for(birthday of birthdaysToLookFor) {
+        waitForBirthday(birthday)
+    }
 }
 
 
