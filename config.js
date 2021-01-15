8c6c85a0c7242911a14315a219f2bf911906a272
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2021-01-15 21:09:17 +0100

    added horoscope and fixed clocks

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     guildID: '774288416467714049',
-    channelID: '775292908881313792'
+    channelID: '775292908881313792',
+    astroChannelID: '799721005592346625'
 }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Discord.Client({disableEveryone : false})
 const Base = require('kf-database')
 const db = new Base({ name: 'birthdays' })
 
+const puppeteer = require('puppeteer')
+
 const config = require('./config')
 let birthdays = require('./birthdays')
 
@@ -53,7 +55,7 @@ client.on('ready', () => {
 
     let now = new Date()
 
-    console.log(birthdaysPerMonth)
+    // console.log(birthdaysPerMonth)
 
     birthdaysToLookFor = birthdaysPerMonth[now.getMonth()].concat(birthdaysPerMonth[(now.getMonth() + 1) % 12])
 
@@ -61,7 +63,12 @@ client.on('ready', () => {
 
     commands = require('./commands/commands')
 
-    dateChecker()
+    let main = async () => {
+        dateChecker()
+        horoscope()
+    }
+
+    main()
 })
 
 client.on('message', (message) => {
@@ -84,13 +91,13 @@ let dateChecker = () => {
     
     let waitForBirthday = (birthday) => {
         let now = new Date()
-        console.log(birthday.date.day === now.getDate() ? ( birthday.date.hour - now.getHours()) * 3600000 + (birthday.date.minute - now.getMinutes()) * 60000 +(birthday.date.seconde - now.getSeconds()) * 1000 : 86400000)
+        console.log(new Date(now.getFullYear(), birthday.date.month, birthday.date.day, birthday.date.hour, birthday.date.minute, birthday.date.seconde).getTime() - new Date().getTime())
         setTimeout(() => {
             console.log('sending message')
             console.log(birthday)
             guild.channels.cache.get(config.channelID).send(`Joyeux anniversaire <@${birthday.id}> <:tada:798513412869980190> <:partying_face:798513412869980190>`)
             birthdaysToLookFor.shift()
-        }, birthday.date.day === now.getDate() ? ( birthday.date.hour - now.getHours()) * 3600000 + (birthday.date.minute - now.getMinutes()) * 60000 +(birthday.date.seconde - now.getSeconds()) * 1000 : 86400000)
+        }, birthday.date.day === now.getDate() ? (new Date(now.getFullYear(), birthday.date.month, birthday.date.day, birthday.date.hour, birthday.date.minute, birthday.date.seconde).getTime() - new Date().getTime()) : 86400000)
     }
 
     // birthdaysToLookFor.unshift({
@@ -98,8 +105,8 @@ let dateChecker = () => {
     //     date: {
     //         month: 0,
     //         day: 15,
-    //         hour: 19,
-    //         minute : 35,
+    //         hour: 21,
+    //         minute : 08,
     //         seconde: 25
     //     }
     // })
@@ -107,6 +114,62 @@ let dateChecker = () => {
     for(birthday of birthdaysToLookFor) {
         waitForBirthday(birthday)
     }
+}
+
+let horoscope = () => {
+    let now = new Date()
+    if(db.get('horoscope') !== now.getDay()) {
+        sendHoroscope()
+    }
+
+    console.log(new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0).getTime() - new Date().getTime())
+    now = new Date()
+    setTimeout(() => {
+        sendHoroscope()
+    }, (new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0).getTime() - new Date().getTime()))
+}
+
+let sendHoroscope = async() => {
+    let browser = await puppeteer.launch()
+    let page = await browser.newPage()
+
+    await page.setDefaultNavigationTimeout(0)
+    await process.setMaxListeners(0)
+
+    let signs = [
+        ['Bélier', 'https://cafeastrology.com/ariesdailyhoroscope.html'],
+        ['Taureau', 'https://cafeastrology.com/taurusdailyhoroscope.html'],
+        ['Gémeaux', 'https://cafeastrology.com/geminidailyhoroscope.html'],
+        ['Cancer', 'https://cafeastrology.com/cancerdailyhoroscope.html'],
+        ['Lion', 'https://cafeastrology.com/leodailyhoroscope.html'],
+        ['Vierge', 'https://cafeastrology.com/virgodailyhoroscope.html'],
+        ['Balance', 'https://cafeastrology.com/libradailyhoroscope.html'],
+        ['Scorpion', 'https://cafeastrology.com/scorpiodailyhoroscope.html'],
+        ['Sagittaire', 'https://cafeastrology.com/sagittariusdailyhoroscope.html'],
+        ['Capricorne', 'https://cafeastrology.com/capricorndailyhoroscope.html'],
+        ['Verseau', 'https://cafeastrology.com/aquariusdailyhoroscope.html'],
+        ['Poisson', 'https://cafeastrology.com/piscesdailyhoroscope.html']
+    ]
+
+    for([sign, url] of signs) {
+        let navPromise = page.waitForNavigation({ waitUntil: "networkidle0" })
+        await page.goto(url)
+        await navPromise
+        content = await page.evaluate(() => [...document.querySelectorAll('main.content div.entry-content p')].map(elt => elt.textContent))
+
+        content = content[3].slice(content[3].indexOf('/>') + 2)
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#E74C3C')
+            .setTitle(sign)
+            .setURL(url)
+            .setAuthor('Alias', 'https://cdn.discordapp.com/avatars/651859468920946738/b2a57698754089238268680b58192e06.png?size=128')
+            .addField("Horoscope du jour", content)
+
+        guild.channels.cache.get(config.astroChannelID).send(embed)
+    }
+
+    db.set('horoscope', new Date().getDay())
 }
 
 
