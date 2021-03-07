6a9d04bd3f4dd62c72562c3f2acbab2b203e058c
Jerryh <supermighty0.cubeway2.mcpe@gmail.com>
2021-03-07 14:46:14 +0100

    changed send command

--- commands/send.js
+++ commands/send.js
@@ -2,17 +2,30 @@ const { DiscordAPIError } = require("discord.js")
 
 module.exports = ({ Discord, client, guild, config, db, message }, args) => {
 
-    args.shift()
-    console.log(message.mentions.channels.first().id)
+    if(!message.guild.members.cache.get(message.author.id).roles.cache.has(config.elythe)) return message.channel.send("T'es pas dans l'élythe, tu mérites pas cette commande")
+
+
+    let channel
+    if(message.mentions.channels.first()) {
+        channel = message.mentions.channels.first()
+        args.shift()
+    } else {
+        channel = message.channel
+    }
+
+    if (args.length === 0) return message.channel.send('Le contenu du message à envoyer est vide (merci Lucas pour la commande)')
 
     const embed = new Discord.MessageEmbed()
         .setColor('#E74C3C')
         .setTitle('Message envoyé')
         .setAuthor('T5', 'https://cdn.discordapp.com/icons/774288416467714049/a002f9e3960163b05d68a86955e98621.webp?size=128')
-        .addField(`${message.author.tag} a demandé l'envoi de ${args.join(' ')} sur le salon ${message.mentions.channels.first().name}`)
+        .addField("Description de l'ordre", `${message.author.tag} a demandé l'envoi de ${args.join(' ')} sur le salon ${channel.name}`, false)
 
     guild.channels.cache.get('809882043709128706').send(embed)
 
-    message.mentions.channels.first().send(args.join(' '))
+    channel.send(args.join(' '))
+
+    message.delete()
+
 
 }
